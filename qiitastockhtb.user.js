6a1a103f997a51a8cd14b1a27d52be3572cf7dcb
Sho Nakajima <s121039@gmail.com>
2019-02-21 20:36:02 +0900

    最初のコミット！

---
+++ qiitastockhtb.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         qiita stock list hatena bookmark
+// @namespace    https://qiita.com
+// @version      1.0.0
+// @description  This script will visually warn you when sending mail to other than the regulated domain in Gmail.
+// @author       Sho Nakajima
+// @include      /^https:\/\/qiita\.com\/stock/
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @downloadURL  
+// @updateURL    
+// @supportURL   
+// ==/UserScript==
+// 
+const hatebapi = "https://b.hatena.ne.jp/entry/jsonlite/";
+const qiitalist = "https://qiita.com";
+
+$(".u-link-no-underline").each(function(index,element) {
+  $.ajax({
+    url : hatebapi,
+    data : {url : encodeURI(qiitalist + $(element).attr("href"))},
+    dataType : "jsonp",
+    success : function(data) {
+      $(element).parent().append(
+        " "
+        + "<a href='"
+        + data.entry_url
+        + "' "
+        + "target='_blank'"
+        + ">"
+        + "<span style='font-weight:bold; color:#f88; font-size:11px;'>"
+        + data.count
+        + "users</span>"
+        + "</a>"
+      );
+    }
+  });
+});
