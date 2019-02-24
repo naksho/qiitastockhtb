3883fd7a9d9e6f6e52c6770313ffd9d91b2aa050
Sho Nakajima <s121039@gmail.com>
2019-02-24 16:18:45 +0900

    jQueryを廃止

--- qiitastockhtb.user.js
+++ qiitastockhtb.user.js
@@ -1,39 +1,41 @@
 // ==UserScript==
 // @name         qiita stock list hatena bookmark
 // @namespace    https://qiita.com
-// @version      1.0.0
-// @description  This script will visually warn you when sending mail to other than the regulated domain in Gmail.
+// @version      1.1.0
+// @description  Qiitaのストック一覧にはてブ数を表示します。
 // @author       Sho Nakajima
 // @include      /^https:\/\/qiita\.com\/stock/
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/fetch-jsonp/1.1.3/fetch-jsonp.min.js
 // @downloadURL  
 // @updateURL    
 // @supportURL   
 // ==/UserScript==
 // 
-const hatebapi = "https://b.hatena.ne.jp/entry/jsonlite/";
+const hatebapi = "https://b.hatena.ne.jp/entry.count";
 const qiitalist = "https://qiita.com";
+const hatebentry = "https://b.hatena.ne.jp/entry/";
 
 const stocks = document.querySelectorAll(".u-link-no-underline");
 
 for ( let element of stocks) {
-  $.ajax({
-    url : hatebapi,
-    data : {url : encodeURI(qiitalist + element.getAttribute("href"))},
-    dataType : "jsonp",
-    success : function(data) {
-      $(element).parent().append(
-        " "
-        + "<a href='"
-        + data.entry_url
-        + "' "
-        + "target='_blank'"
-        + ">"
-        + "<span style='font-weight:bold; color:#f88; font-size:11px;'>"
-        + data.count
-        + "users</span>"
-        + "</a>"
-      );
-    }
+  let params = new URLSearchParams();
+  params.set('url', encodeURI(qiitalist + element.getAttribute("href")));
+  fetchJsonp(hatebapi + '?' + params.toString())
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(responseText) {
+    element.insertAdjacentHTML('afterend',
+    " "
+    + "<a href='"
+    + encodeURI(hatebentry + qiitalist + element.getAttribute("href"))
+    + "' "
+    + "target='_blank'"
+    + ">"
+    + "<span style='font-weight:bold; color:#f88; font-size:11px;'>"
+    + responseText
+    + "users</span>"
+    + "</a>"
+    );
   });
 };
